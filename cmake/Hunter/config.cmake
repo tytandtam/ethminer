9d482b271e2b1125d77edca67c39e9b3aa7f797e
tytandtam <tytandtam@gmail.com>
2022-08-02 20:39:55 +0300

    ethereum classic branch

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -5,3 +5,4 @@ hunter_config(
     SHA1 9dc9d0ac0de28f41bdf3b7545df5f41536cc5a79
     URL https://github.com/tytandtam/boost/archive/v1.66.0-macos.tar.gz
 )
+hunter_config(ethash GIT_SUBMODULE "cmake/ethash")
