43cdeacf5523b55f05ec3173ad8769516f14f6de
tytandtam <tytandtam@gmail.com>
2022-08-02 20:22:47 +0300

    macOS patch

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,2 +1,7 @@
 hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=OFF CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
-hunter_config(Boost VERSION 1.66.0)
+hunter_config(
+    Boost
+    VERSION 1.66.0-macos
+    SHA1 9dc9d0ac0de28f41bdf3b7545df5f41536cc5a79
+    URL https://github.com/tytandtam/boost/archive/v1.66.0-macos.tar.gz
+)
